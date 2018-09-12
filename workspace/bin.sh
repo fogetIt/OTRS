d15203e46a6874b176da1c6c73be511b64556cd2
forgetIt <2271404280@qq.com>
2018-09-12 16:32:23 +0800

    123

--- workspace/bin.sh
+++ workspace/bin.sh
@@ -4,6 +4,6 @@ docker-compose down
 docker-compose rm -f -v
 echo 'y' | docker volume prune
 docker-compose pull
-# docker-compose up -d
-# docker logs otrs --follow
+docker-compose up -d
+docker logs otrs --follow
 # localhost:8080/otrs/installer.pl
