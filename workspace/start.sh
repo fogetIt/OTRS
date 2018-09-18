9f4735cac7c4b1be32fdc48bf333bc69051f90d6
forgetIt <2271404280@qq.com>
2018-09-18 13:20:16 +0800

    123

--- workspace/start.sh
+++ workspace/start.sh
@@ -4,7 +4,7 @@ export OTRS_USER=otrs
 export OTRS_VERSION=${1}
 docker-compose down
 if [ ${OTRS_VERSION%%.*} == '5' ]; then
-    # otrs-5.0.16/5.0.22/5.0.26
+    # otrs-5.0.16/5.0.22/5.0.26/5.0.29
     pushd build
         if [[ ! -d "otrs-${OTRS_VERSION}" ]]; then
             mkdir "otrs-${OTRS_VERSION}"
@@ -26,4 +26,5 @@ docker-compose rm -f -v
 echo 'y' | docker volume prune
 docker-compose up -d
 docker logs otrs --follow
-# otrs/installer.pl
+# /otrs/installer.pl
+# /otrs/index.pl
